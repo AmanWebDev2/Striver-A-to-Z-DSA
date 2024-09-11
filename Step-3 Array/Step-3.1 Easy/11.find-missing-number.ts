0d19de7106d98645136f87e09277a3a5f738f21c
AmanWebdev2 <webdevaman2@gmail.com>
2024-09-11 17:43:02 +0530

    added better approach

--- Step-3 Array/Step-3.1 Easy/11.find-missing-number.ts
+++ Step-3 Array/Step-3.1 Easy/11.find-missing-number.ts
@@ -1,4 +1,6 @@
 function findMissingNumBrute(arr:number[],N:number):number{
+    // T.C = O(n^2);
+    // S.C = O(1);
     let missingNum = -1;
     for(let i=1; i<=N; i++) {
         let isFound = false;
@@ -17,6 +19,29 @@ function findMissingNumBrute(arr:number[],N:number):number{
 }
 
 const arr = [1,2,3,4,5,6,8];
-const N = arr.length;
+const N = arr.length+1;
 
-console.log(findMissingNumBrute(arr,N+1));
+// console.log(findMissingNumBrute(arr,N+1));
+
+function findMissingNumBetter(arr:number[],N:number):number {
+    // T.C => O(N) + O(N);
+    // S.C => O(N)
+
+    const hash = new Array(N+1).fill(0);
+    let ans = -1;
+
+    for(let i=0; i<N-1; i++) {
+        hash[arr[i]] = 1;
+    }
+
+    for(let i=1; i<hash.length; i++) {
+        if(hash[i] == 0) {
+            ans = i;
+            break;
+        }
+    }
+
+    return ans;
+}
+
+console.log(findMissingNumBetter(arr,N+1));
